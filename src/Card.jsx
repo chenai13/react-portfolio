4c143da9ce985d137f6dc61bb6dd14221bb0d5aa
Chenai <186724093+chenai13@users.noreply.github.com>
2026-02-11 17:42:30 +0000

    Fix layout alignment and white space issues

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,21 +3,19 @@ import "./App.css";
 function Card(props) {
   return (
     <>
-    <a href={props.link} target="_blank" rel="noopener noreferrer">
-      <div className="bg-[var(--color-card)] hover:bg-[var(--color-card-hover)] text-text p-5 w-full max-w-[500px]  transition delay-105 duration-130 ease-in-out hover:-translate-y-1 hover:scale-102 hover:shadow-sm rounded-md drop-shadow-sm font-heebo text-sm font-light">
-        <div className="flex justify-between w-57">
-          <h1 className="pb-5 pt-5 font-bold text-lg font-inconsolata">
-            {props.title}
-          </h1>
-          <img className="size-14" src={props.img}></img>
+      <a href={props.link} target="_blank" rel="noopener noreferrer">
+        <div className="bg-[var(--color-card)] hover:bg-[var(--color-card-hover)] text-text p-5  transition delay-105 duration-130 ease-in-out hover:-translate-y-1 hover:scale-102 hover:shadow-sm rounded-md drop-shadow-sm font-heebo text-sm font-light">
+          <div className="flex justify-between w-57">
+            <h1 className="pb-5 pt-5 font-bold text-lg font-inconsolata">
+              {props.title}
+            </h1>
+            <img className="size-14" src={props.img}></img>
+          </div>
+          <div className="pb-8 flex justify-between w-50 ">
+            <p>{props.date}</p>|<p>{props.prg}</p>
+          </div>
+          <p className="pb-5">{props.p}</p>
         </div>
-        <div className="pb-8 flex justify-between w-50 ">
-          <p>{props.date}</p>|<p>{props.prg}</p>
-        </div>
-        <p className="pb-5">{props.p}
-        </p>
-       
-      </div>
       </a>
     </>
   );
--- src/Hero.jsx
+++ src/Hero.jsx
@@ -9,69 +9,75 @@ import LinkDark from "./images/linkedin-dark.svg";
 function Hero() {
   return (
     <>
-      <section className="text-text font-inconsolata p-3 md:text-lg bg-background">
-        <div className="p-2 pb-15 pt-10 text-center md:pr-30 md:pt-25 md:pl-30 lg:text-left lg:pl-100 lg:pr-100">
-          <div className="p-8 text-3xl md:text-4xl md:pl-0">
-            <h1 className="font-bold">Hi, Im Chenai</h1>
-            <h2 className="font-normal">Frontend Developer</h2>
-          </div>
-          <p className="text-sm font-heebo pb-5">
-            Hi! Welcome to my portfolio. I love building websites related to my
-            hobbies, and this is where I’m tracking my progress. Stick around,
-            more projects coming soon!
-          </p>
-          <a
-            href="https://docs.google.com/document/d/1rFDvnuaxqHY8_WBJCYKoQ1QW6OXR1yqD/edit?usp=sharing&ouid=102378930502161151701&rtpof=true&sd=true"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="bg-[var(--color-button)] hover:bg-[var(--color-button-hover)] inline-block w-50 transition duration-150 ease-in-out hover:scale-105 font-heeborounded-md text-white py-2 px-4 border-4 border-transparent text-center"
-          >
-            My Resume
-          </a>
-          <div className="flex justify-center mt-4 space-x-3 lg:justify-start">
+    <main className="min-h-screen flex flex-col bg-background">
+      <section className="text-text font-inconsolata bg-background min-w-80 ">
+        <div className="max-w-screen-lg mx-auto px-6 lg:px-20">
+          <div className="flex flex-col items-center lg:items-start text-center lg:text-left pt-10 pb-15 md:pt-25">
+            <div className="py-8 text-3xl md:text-4xl lg:pl-0">
+              <h1 className="font-bold">Hi, I'm Chenai</h1>
+              <h2 className="font-normal text-2xl md:text-3xl">
+                Frontend Developer
+              </h2>
+            </div>
+
+            <p className="text-sm font-heebo pb-5 max-w-sm mx-auto lg:mx-0">
+              Hi! Welcome to my portfolio. I love building websites related to
+              my hobbies, and this is where I’m tracking my progress. Stick
+              around, more projects coming soon!
+            </p>
+
             <a
-              href="https://github.com/chenai13"
+              href="https://docs.google.com/document/d/1rFDvnuaxqHY8_WBJCYKoQ1QW6OXR1yqD/edit?usp=sharing&ouid=102378930502161151701&rtpof=true&sd=true"
               target="_blank"
               rel="noopener noreferrer"
+              className="bg-[var(--color-button)] hover:bg-[var(--color-button-hover)] inline-block w-50 transition duration-100 font-heebo rounded-md text-white py-2 px-4 border-4 border-transparent text-center"
             >
-              <img
-                src={githubLight}
-                className="w-8 block dark:hidden [var(--opacity-icon-hover)] icon"
-                alt="GitHub icon"
-              />
-              <img
-                src={githubDark}
-                className="w-8 hidden dark:block [var(--opacity-icon-hover)] icon"
-                alt="GitHub icon"
-              />
+              My Resume
             </a>
-            <a
-              href="https://www.linkedin.com/in/chenai-lamarque-090802214/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src={LinkLight}
-                alt="LinkedIn icon "
-                className="w-8 block dark:hidden [var(--opacity-icon-hover)] icon"
-              />
-              <img
-                src={LinkDark}
-                alt="LinkedIn icon"
-                className="w-8 hidden dark:block [var(--opacity-icon-hover)] icon"
-              />
-            </a>
+
+            <div className="flex justify-center mt-6 space-x-3 lg:justify-start">
+              <a
+                href="https://github.com/chenai13"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={githubLight}
+                  className="w-8 block dark:hidden [var(--opacity-icon-hover)] icon"
+                  alt="GitHub icon"
+                />
+                <img
+                  src={githubDark}
+                  className="w-8 hidden dark:block [var(--opacity-icon-hover)] icon"
+                  alt="GitHub icon"
+                />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/chenai-lamarque-090802214/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={LinkLight}
+                  alt="LinkedIn icon "
+                  className="w-8 block dark:hidden [var(--opacity-icon-hover)] icon"
+                />
+                <img
+                  src={LinkDark}
+                  alt="LinkedIn icon"
+                  className="w-8 hidden dark:block [var(--opacity-icon-hover)] icon"
+                />
+              </a>
+            </div>
           </div>
         </div>
       </section>
-      <div className="bg-projects text-text font-inconsolata py-10">
+      <div className=" flex-grow bg-projects text-text font-inconsolata py-10 min-w-80">
         <div className="max-w-5xl mx-auto px-4 sm:px-6 md:px-8 flex flex-col">
-          {/* Section Title */}
           <p className="text-xl font-semibold mb-6 text-center lg:text-left">
             My projects
           </p>
 
-          {/* Card Row */}
           <div className="flex flex-col items-center space-y-6 md:space-y-6 lg:flex-row lg:justify-center lg:space-y-0 lg:space-x-6">
             <Card
               link="https://chenai13.github.io/crochet-assistant/"
@@ -95,6 +101,7 @@ function Hero() {
           </div>
         </div>
       </div>
+      </main>
     </>
   );
 }
